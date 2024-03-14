65d43c1049909c4979806b180ddaf8606d73a05c
nadaalaa467 <n192004a@gmail.com>
2024-03-14 15:17:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./App.css"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
+import Home from "./pages/home/Home"
+import Profile from "./pages/profile/Profile"
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      {/*<Profile />*/}
+      {/*<Login />*/}
+      {/*<Register />*/}
+    </>
+  )
+}
+
+export default App
